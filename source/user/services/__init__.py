ef90c3b09deabddef7809d03cb17d8aceb1423fa
DP <dancewithpeng@gmail.com>
2018-09-12 21:34:02 +0800

    添加跨域通用请求头 (#2)

--- source/user/services/__init__.py
+++ source/user/services/__init__.py
@@ -1,10 +1,18 @@
 # 服务层
-from flask import Flask
+from flask import Flask, make_response, json
 from flask_restful import Api
 
 def init_app(app: Flask) : 
 
     api = Api(app)
+
+    @api.representation('application/json')
+    def output_json(data, code, headers=None):
+        resp = make_response(json.dumps(data), code)        
+        new_headers = {'Access-Control-Allow-Origin': '*'}
+        new_headers.update(headers or {})
+        resp.headers.extend(new_headers)
+        return resp
 
     from .signup import Signup    
     from .password_auth import PasswordAuth
